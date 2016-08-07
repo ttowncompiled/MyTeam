8390a39188a38268f60ac81e4a1a102fe20171ea
Ian Riley <ian.riley.915@gmail.com>
2016-08-06 21:40:53 -0500

    Connects login page to Firebase project.

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 /*
   Generated class for the LoginPage page.
@@ -12,8 +13,10 @@ import { NavController } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(private navCtrl: NavController) {
-
+  constructor(private navCtrl: NavController, public af: AngularFire) {
+    af.database.list('items').subscribe((items: any[]) => {
+      console.log(items);
+    });
   }
 
 }
